558fb84d65b40ec501d8c9d454dad5d149959c97
mildhouse <miloslav.kostir@gmail.com>
2014-03-01 07:47:31 -0800

    Remake, compatibility with Nette 2.0.* and 2.1.*

--- DialogControl.php
+++ DialogControl.php
@@ -1,144 +1,174 @@
 <?php
 
 /**
- * DialogControl resi problem s formularem v modalnim okne.
- * Pro zobrazeni modalniho okna nepouzivat klasicke signaly zobrazOkno! (tzn. www.stranka.cz?do=zobrazOkno),
- * ale misto toho pouzit parametr jiny (pseudo signal), param. 'do' potom prevezme formular a nedoslo by k zobrazeni okna.
- * 
- * @author  Miloslav Koštíř
- *    
+ * DialogControl - modal window for Nette framework 2.x
+ *
+ * @author			Miloslav Koštíř
+ * @copyright		Copyright (c) 2013 Miloslav Koštíř
+ * @license     New BSD Licence
  */
  
-namespace Components;      
+namespace Mildhouse;
 
-class DialogControl extends BaseControl {
+use Nette;      
 
-		/** @var array */
-		protected $html;
+class DialogControl extends Nette\Application\UI\Control
+{
+	
+	/** @var string */
+	protected $templatesDir;
 		
-		/** @var	string */ 
-		protected $text;
-		
-		/** @var	string */
-		protected $class;
-		
-		/** @var	string */
-		protected $el;
-		
-		/** @var	string */
-		protected $file;
-		
-		/** @var	mixed */
-		protected $control;
-		
-		/** @var	string */
-		protected $name;
-		
-		/** @var	string */
-		protected $width = '400px';
-		
-		/** @var bool */
-		private $open = FALSE;
-		
-		/** @var array */
-		public $onStore;
-		
-		/** @var array */
-		public $onRestore;
-		
-		/** @var array */
-		public $onClose;
-				
-		/** @var string*/
-		private $newToken;
-		
-		/** @NSessionSection */
-		private $storage;
-		
-		/** @var string */
-		private $signal;
-		
-		/** @var array */
-		private $parameters;
-		
+	/** @var array */
+	protected $html;
+	
+	/** @var	string */
+	protected $file;
+	
+	/** @var	array */
+	protected $control;
+	
+	/** @var	string */
+	protected $block;
+	
+	/** @var	string */
+	protected $width = '400px';
+	
+	/** @var bool */
+	protected $open;
+	
+	/** @var bool */
+	protected $storing;
+	
+	/** @var bool */
+	protected $restoring;
+	
+	/** @var array */
+	public $onClose;
+			
+	/** @var string*/
+	private $newToken;
+	
+	/** @var Nette\Http\SessionSection */
+	private $storage;
+	
+	/** @var string */
+	private $signal;
+	
+	/** @var array */
+	private $parameters;
 	
 	
 	/**
-	 * Spusti se pri pripojeni k presenteru	
-	 * @param \Nette\Application\UI\Presenter $presenter
-	 */
-	protected function attached($presenter)
+	 * @param string
+	 */	 	
+	public function __construct($templatesDir = NULL)
 	{
-		parent::attached($presenter);
-
-		if(!$presenter instanceof \Nette\Application\UI\Presenter) return;
+		$this->templatesDir = $templatesDir;
+	}
+	
+	
+	/**
+	 * Self-setting.
+	 * @return void
+	 */	 	  	
+	protected function setUp()
+	{
+		$presenter = $this->presenter;
 		
-		if($presenter->isAjax()){
+		if($presenter->isAjax())
+		{
 			$this->invalidateControl();
 		}
 
 		$this->signal = $this->parent->getParameter('dialogDo');
 		$this->newToken = $presenter->name.$presenter->action;
 		$this->storage = $presenter->getSession('dialog');
-		$this->parameters = $presenter->getParameter();
-		unset($this->parameters["do"]);
+		$this->parameters = method_exists($presenter, 'getParameters') ? $presenter->getParameters() : $presenter->getParameter();
+		unset($this->parameters['do']);
 
 		$this->checkToken();
 	}
 	
-	protected function configure($presenter){
 	
+	/**
+	 * Configuration for advanced use. Use it instead of callback
+	 * @param Nette\Application\UI\Presenter
+	 * @return void	 
+	 */	 	 	
+	protected function configure($presenter)
+	{
 	}
 	
 	
-	private function checkToken(){
-		if(isset($this->storage->token) AND $this->storage->token != $this->newToken){	
+	/**
+	 * Recognizes another page by token and closes the window.
+	 * @return void
+	 */	 	 	
+	private function checkToken()
+	{
+		if(isset($this->storage->token) AND $this->storage->token != $this->newToken)
+		{	
 			$this->storage->remove();
 			$this->close();
 		}
 	}
 	
-	private function setToken(){
-		if(!isset($this->storage->token)){
+	
+	/**
+	 * Sets the token
+	 * @return void
+	 */	 	 	
+	private function setToken()
+	{
+		if(!isset($this->storage->token))
+		{
 			$this->storage->token = $this->newToken;
 		}
 	}
 	
 	
 	/**
-	 * Inicializace
-	 * @param  string  $signal
-	 * @param  Nette\Callback  $callback
+	 * Initialization. 
+	 * @param string $signal
+	 * @param Nette\Callback $callback
 	 * @return bool
 	 */	 	 	 	 	
-	public function init($signal, $callback = NULL){
-		// neotestovano
-		if(($signal instanceof \Nette\Callback OR is_callable($signal)) AND $callback === NULL){
+	public function init($signal = NULL, $callback = NULL)
+	{
+		$this->setUp();
+
+		if(($signal instanceof Nette\Callback OR is_callable($signal)) AND $callback === NULL)
+		{
 			$callback = $signal;
 			$signal = NULL;
 		}
 		
-		if($signal === NULL OR (!empty($this->signal) AND $this->signal == $signal)){			
+		if($signal === NULL OR (!empty($this->signal) AND $this->signal == $signal))
+		{
+			$this->storing = TRUE;			
 			$this->storage->handle = $this->signal;
 			$this->storage->args = $this->parameters;
 			$this->setToken();
 			$this->configure($this->presenter);
-			$this->onStore($this);
 			$return = TRUE;
 		}
-		elseif(!empty($this->storage->handle) AND $this->storage->handle == $signal){
+		elseif(!empty($this->storage->handle) AND $this->storage->handle == $signal)
+		{
+			$this->restoring = TRUE;
 			$this->configure($this->presenter);				
-			$this->onRestore($this);
 			$return = TRUE;
 		}
-		else{
+		else
+		{
 			return FALSE;
 		}
 		
-		if($callback instanceof \Nette\Callback){
+		if($callback instanceof Nette\Callback)
+		{
 			$callback->invoke($this);
 		}		
-		elseif(is_callable($callback)){
+		elseif(is_callable($callback))
+		{
 			call_user_func($callback, $this);
 		}
 				
@@ -147,11 +177,12 @@ class DialogControl extends BaseControl {
 	
 	
 	/**
-	 * Vrati argumenty z URL
+	 * Gets URL parameters
 	 * @param  string  $name
-	 * @return string
+	 * @return string|array
 	 */	 	 	 	
-	public function getArgs($name = NULL){
+	public function getArgs($name = NULL)
+	{
 		if(isset($name) AND isset($this->storage->args[$name])) return $this->storage->args[$name];
 		elseif(isset($name)) return NULL;
 		else return $this->storage->args;
@@ -159,11 +190,13 @@ class DialogControl extends BaseControl {
 	
 	
 	/**
-	 * Vrati nazev pseudo-handl-funkce
+	 * Gets name of pseudo handl function
 	 * @return  string
 	 */	 	   
-	public function getHandle(){ 
-		if(isset($this->storage->handle)){	
+	public function getHandle()
+	{ 
+		if(isset($this->storage->handle))
+		{	
 		  return $this->storage->handle;
 		}
 		else return NULL;  				
@@ -171,62 +204,100 @@ class DialogControl extends BaseControl {
 	
 	
 	/**
-	 * Zjisti, zda je dialog zobrazen
+	 * Is the window open?
 	 * @return  bool
 	 */
-	public function isOpen(){
-		return $this->open;
+	public function isOpen()
+	{
+		return (bool) $this->open;
 	}
 	
 	
 	/**
-	 * Vykreslí text v elementu $el s třídou $class
+	 * Is the window storing to session now?
+	 * @return  bool
+	 */
+	public function isStoring()
+	{
+		return (bool) $this->storing;
+	}
+	
+	
+	/**
+	 * Is the window restoring from session now?
+	 * @return  bool
+	 */
+	public function isRestoring()
+	{
+		return (bool) $this->restoring;
+	}
+	
+	
+	/**
+	 * Adds message with text $text in HTML element $el with css class $class for later render
 	 * @param 	string 	$text
 	 * @param		string 	$class
 	 * @param  	string 	$el
-	 * @return 	DialogControl
+	 * @return 	self
 	 */
-	public function message($text, $class = NULL, $el = 'div'){
-		$this->text = $text;
-		$this->class = $class;
-		$this->el = $el;
+	public function message($text, $class = NULL, $el = 'div')
+	{
+		$html = Nette\Utils\Html::el($el)->setText($text)->setClass($class);
+		$this->html($html);
 		return $this;
 	}
 	
 	
 	/**
-	 * Vlozi do dialogoveho okna objekt \Nette\Utils\Html a vykresli
+	 * Adds Nette\Utils\Html for later render
 	 * @param   Nette\Utils\Html  $html
-	 * @return  DialogControl
+	 * @return  self
 	 */	 	 	 	
-	public function html(\Nette\Utils\Html $html){
+	public function html(Nette\Utils\Html $html)
+	{
 		$this->html[] = $html;
 		return $this;
 	}
 	
 	
 	/**
-	 * Vykreslí předem nadefinovaný blok $name ze souboru $file
-	 * @param		string	$name
-	 * @param		string	$file
-	 * @return	DialogControl
+	 * Sets block $block from template $file for later render
+	 * @param		string	$block
+	 * @param		string	$file  If NULL, is's trying to find template by $block in $templatesDir property 
+	 * @return	self
+	 * @throws 	Nette\InvalidArgumentException If both $file and DialogControl::$templatesDir are empty 
 	 */		 		 		 		 		     
-	public function block($name, $file = NULL){
-		$this->name = $name;
-		$this->setFile($file);
+	public function block($block, $file = NULL)
+	{
+		$this->block = $block;
+		if(!isset($file) AND isset($this->templatesDir))
+		{
+			$this->file = $this->templatesDir . '/' . $block . '.latte';
+		}
+		elseif(isset($file))
+		{
+			$this->file = $file;
+		}
+		else
+		{
+			throw new Nette\InvalidArgumentException('No template path defined. Second argument must not be NULL if property '.__CLASS__.'::templateDir is empty.');
+		}
 		return $this;
 	}
 	
+	
 	/**
-	 * Vloží do dialogového okna hotovou (vytvorenou) komponentu a rovnou vykresli
-	 * zadavat hotovou komponentu - $presenter["controlName"]     
-	 * @param		mixed			$control   		
-	 * @param		bool			$addProtection
-	 * @return	DialogControl
+	 * Adds control for instant render and adds XSRF protection   
+	 * @param		mixed	$control   		
+	 * @param		bool	$addProtection
+	 * @return	self
 	 */
-	public function control($control, $addProtection = TRUE){
-		if($addProtection === TRUE){
-			if(!isset($control[\Nette\Application\UI\Form::PROTECTOR_ID]) AND method_exists($control, "addProtection")) {
+	public function control($control, $addProtection = TRUE)
+	{
+		if($addProtection === TRUE)
+		{
+			if(!isset($control[Nette\Application\UI\Form::PROTECTOR_ID]) AND method_exists($control, 'addProtection')) 
+			{
 				$control->addProtection();
 			}					
 		}
@@ -236,14 +307,17 @@ class DialogControl extends BaseControl {
 	}
 	
 	/**
-	 * Vytvoří v dialogovém okně komponentu, nebude hned vykreslena, musi se volat v sablone {control controlName}
-	 * @param		string		$name
-	 * @param		mixed			$control 	Komponenta, formular, atd.
-	 * @return	DialogControl
+	 * Creates component in dialog window control which will be possible to use in block via macro {control controlName}
+	 * @param		string	$name Name of control
+	 * @param		mixed		$control Component factory eg. createComponentSomeControl()
+	 * @param		bool		$addProtection	 
+	 * @return	self
 	 */		 		 		 		     
-	public function createControl($name, $control = NULL, $addProtection = TRUE){
+	public function createControl($name, $control = NULL, $addProtection = TRUE)
+	{
 		$this[$name] = $control;
-		if($addProtection === TRUE AND !isset($control[\Nette\Application\UI\Form::PROTECTOR_ID]) AND method_exists($control, "addProtection")){ 
+		if($addProtection === TRUE AND !isset($control[Nette\Application\UI\Form::PROTECTOR_ID]) AND method_exists($control, 'addProtection'))
+		{ 
 			$this[$name]->addProtection();
 		}
 	
@@ -251,80 +325,77 @@ class DialogControl extends BaseControl {
 	}
 	
 	/**
-	 * Nastaví šířku okna
-	 * @param		string	$width		Default '400px'
-	 * @return	DialogControl
+	 * Sets width of window
+	 * @param		string	$width	Default '400px'
+	 * @return	self
 	 */
-	public function setWidth($width){
+	public function setWidth($width)
+	{
 		$this->width = $width;
 		return $this;
 	}
 	
 	/**
-	 * Otevře dialogové okno
+	 * Opens the window
 	 * @return void
 	 */
-	public function open(){ 
+	public function open()
+	{ 
 		$this->open = TRUE;      
 	}
 	
 	
 	/**
-	 * Zavře dialogové okno
+	 * Closes the window
+	 * @param  string $location	 
 	 * @return void
 	 */
-	public function close($location = 'this'){ 
+	public function close($location = NULL)
+	{ 
 		$this->storage->remove();
 		$this->open = FALSE;		
-		if(!$this->presenter->isAjax()){
+		if(!$this->presenter->isAjax())
+		{
 			if(isset($location)) $this->presenter->redirect($location);
+			else $this->presenter->redirect('this');
 		}
 	}
 
 	
-	public function handleClose($location){
-		// vyvoláme události přidané do onClose
+	/**
+	 * Closes the window
+	 * @param  string $location	 
+	 * @return void
+	 */	
+	public function handleClose($location = NULL){
 		$this->onClose($this);
-		if(empty($location)) $location = 'this';
 		$this->close($location);
+	}
+
+
+	/**
+	 * Sets params in template
+	 * return void
+	 */	 	 	
+	public function setTemplateParams() {			
+		$this->template->componentName = $this->getName();		
+		$this->template->width = $this->width;    
+		$this->template->file = $this->file;
+		$this->template->control = $this->control;
+		$this->template->html = $this->html;
+		$this->template->block = $this->block;
+		$this->template->open = $this->isOpen();
 	}
 	
 	
 	/**
-	 * Nastaví cestu k souboru s šablonou
-	 * @param  string	 $file
-	 * @return DialogControl
-	 */		 		 		     
-	private function setFile($file = NULL){
-		if(!isset($file)){
-			$p = array_reverse(explode(":", $this->presenter->name));
-			$file = APP_DIR."/";
-			$file .= isset($p[1]) ? $p[1]."Module/templates/" : "templates/";
-			$file .= $p[0]."/".$this->presenter->action.".latte";
-		}
-		$this->file = $file;
-		return $this;
-	}
-
-
-
-
-
-	public function render() {			
-		$this->template->componentName = $this->getName();
-		$this->template->setFile(dirname(__FILE__) . '/DialogControl.latte');
+	 * Render. If you rewrite this method (change the file path) don't forget call setTemplateParams().
+	 */	 	
+	public function render() {
+		$this->setTemplateParams();			
 		
-		$this->template->width = $this->width;
-		$this->template->text = $this->text;
-		$this->template->class = $this->class;
-		$this->template->el = $this->el;    
-		$this->template->file = $this->file;
-		$this->template->control = $this->control;
-		$this->template->html = $this->html;
-		$this->template->name = $this->name;
-		$this->template->close = !$this->isOpen();
-
+		$this->template->setFile(__DIR__ . '/DialogControl.latte');
 		$this->template->render();
 	}
-
+	
 }
